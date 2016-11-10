1d4ce799f7e1e49c8679f90cb3ee01ab7746b53d
Mayron Medeiros <mayronmedeiros@live.com>
2016-11-10 16:45:57 -0200

    first commit

---
+++ config/database.js
@@ -0,0 +1,6 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb://admin:pass@ds147267.mlab.com:47267/passport_example' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+
+};
